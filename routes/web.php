5b39f00c4f8b6aef5183399a55c6eaeffedd0e96
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2025-02-21 16:04:34 +0000

    création de la méthodes d'ajout d'un commentaire

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::post('articles/{id}',[CommentController::class,'addComment']);
 
 Route::get('/', function () {
     return view('welcome');
